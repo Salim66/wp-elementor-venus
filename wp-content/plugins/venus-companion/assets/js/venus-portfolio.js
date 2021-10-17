cc3ce44971cc84fbe1571fb1696a0156767be8ef
Salim66 <salimhasanriad8@gmail.com>
2021-10-17 13:29:14 +0600

    Portfolio element, masonry grid render by javascript

---
+++ wp-content/plugins/venus-companion/assets/js/venus-portfolio.js
@@ -0,0 +1,25 @@
+;(function($){
+    $(document).ready(function(){
+        if(window.elementorFrontend){
+            elementorFrontend.hooks.addAction('frontend/element_ready/portfolio.default', function(scope, $){
+                var $portfolio = $(scope).find('.portfolio-grid');
+                if($.fn.imagesLoaded && $portfolio.length > 0){
+                    imagesLoaded($portfolio, function(){
+                        $portfolio.isotope({
+                            itemSelector: '.portfolio-item',
+                            filters: '*'
+                        });
+                        $(window).trigger("resize");
+                    });
+                }
+
+                $(scope).find('.portfolio-filter').on('click', 'a', function(e){
+                    e.preventDefault();
+                    $(this).parent().addClass('active').siblings().removeClass('active');
+                    var filterValue = $(this).attr('data-filter');
+                    $portfolio.isotope({filter: filterValue});
+                });
+            });
+        }
+    });
+})(jQuery);
