595d3f7565f9909873874ec2e153498f0f9640d9
Salim66 <salimhasanriad8@gmail.com>
2021-10-18 17:15:32 +0600

    Team page minimul team member widget

--- wp-content/plugins/venus-companion/plugin.php
+++ wp-content/plugins/venus-companion/plugin.php
@@ -11,6 +11,7 @@ use VenusCompanion\Widgets\Services;
 use VenusCompanion\Widgets\Bullet_list;
 use VenusCompanion\Widgets\Client_Logo;
 use VenusCompanion\Widgets\Team_Carousel;
+use VenusCompanion\Widgets\Minimul_Team_Member;
 /**
  * Class Plugin
  *
@@ -83,6 +84,7 @@ class VenusPlugin {
 		require_once( __DIR__ . '/widgets/bullet-list.php' );
 		require_once( __DIR__ . '/widgets/client-logo.php' );
 		require_once( __DIR__ . '/widgets/team-carousel.php' );
+		require_once( __DIR__ . '/widgets/minimul-team-member.php' );
 	}
 
 	/**
@@ -109,6 +111,7 @@ class VenusPlugin {
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Bullet_list());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Client_Logo());
 		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Team_Carousel());
+		\Elementor\Plugin::instance()->widgets_manager->register_widget_type( new Minimul_Team_Member());
 	}
 
 	/**
---
+++ wp-content/plugins/venus-companion/widgets/minimul-team-member.php
@@ -0,0 +1,189 @@
+<?php
+namespace VenusCompanion\Widgets;
+
+use Elementor\Widget_Base;
+use Elementor\Controls_Manager;
+
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+
+/**
+ * Elementor Hello World
+ *
+ * Elementor widget for hello world.
+ *
+ * @since 1.0.0
+ */
+class Minimul_Team_Member extends Widget_Base {
+
+	/**
+	 * Retrieve the widget name.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget name.
+	 */
+	public function get_name() {
+		return 'minimul-team-member';
+	}
+
+	/**
+	 * Retrieve the widget title.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget title.
+	 */
+	public function get_title() {
+		return __( 'Minimul Team Member', 'venus-companion' );
+	}
+
+	/**
+	 * Retrieve the widget icon.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return string Widget icon.
+	 */
+	public function get_icon() {
+		return ' eicon-preferences';
+	}
+
+	/**
+	 * Retrieve the list of categories the widget belongs to.
+	 *
+	 * Used to determine where to display the widget in the editor.
+	 *
+	 * Note that currently Elementor supports only one category.
+	 * When multiple categories passed, Elementor uses the first one.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget categories.
+	 */
+	public function get_categories() {
+		return [ 'general' ];
+	}
+
+	/**
+	 * Retrieve the list of scripts the widget depended on.
+	 *
+	 * Used to set scripts dependencies required to run the widget.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 *
+	 * @return array Widget scripts dependencies.
+	 */
+	// public function get_script_depends() {
+	// 	return [ 'venus-companion' ];
+	// }
+
+	/**
+	 * Register the widget controls.
+	 *
+	 * Adds different input fields to allow the user to change and customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _register_controls() {
+		$this->start_controls_section(
+			'section_content',
+			[
+				'label' => __( 'Content', 'venus-companion' ),
+			]
+		);
+
+		$this->add_control(
+			'photo',
+			[
+				'label' => __( 'Photo', 'venus-companion' ),
+				'type' => Controls_Manager::MEDIA,
+			]
+		);
+
+		$this->add_control(
+			'name',
+			[
+				'label' => __( 'name', 'venus-companion' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$this->add_control(
+			'designation',
+			[
+				'label' => __( 'Designation', 'venus-companion' ),
+				'type' => Controls_Manager::TEXT,
+			]
+		);
+
+		$this->end_controls_section();
+
+		$this->start_controls_section(
+			'section_style',
+			[
+				'label' => __( 'Style', 'venus-companion' ),
+				'tab' => Controls_Manager::TAB_STYLE,
+			]
+		);
+
+		$this->end_controls_section();
+	}
+
+	/**
+	 * Render the widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function render() {
+		$settings = $this->get_settings_for_display();
+
+		?>
+		<div class="card border-0 mb-4 box-hover">
+			<img class="card-img-top" src="<?php echo esc_html($settings['photo']['url']); ?>" alt="">
+			<div class="team-footer border-0">
+				<div class="d-flex align-itmes-center justify-content-between">
+					<div>
+						<?php echo esc_html($settings['name']); ?>
+						<span class="text-muted d-block font-size-14">
+							<?php echo esc_html($settings['designation']); ?>
+						</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<?php 
+	}
+
+	/**
+	 * Render the widget output in the editor.
+	 *
+	 * Written as a Backbone JavaScript template and used to generate the live preview.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access protected
+	 */
+	protected function _content_template_() {
+		?>
+		<div class="title">
+			{{{ settings.title }}}
+		</div>
+		<?php
+	}
+}
